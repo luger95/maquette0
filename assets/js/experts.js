5c72f89c0cdb4c1b1967d626dfc914d97478c7c3
Alexis Deschamps <22308850t>
2024-10-17 10:31:23 +0200

    Renommage de pad en zerosBefore, ajout de documentation, correction mineures

--- assets/js/experts.js
+++ assets/js/experts.js
@@ -1,3 +1,6 @@
+/**
+ * Initialisation
+ */
 const PIXELPERSLIDE = -470;
 let slideNumber = 1
 
@@ -6,8 +9,14 @@ let itemIndex = document.getElementById("item-index")
 let expertiseRightArrow = document.getElementById("expertise-right-arrow");
 let expertiseLeftArrow = document.getElementById("expertise-left-arrow");
 
-itemIndex.innerText = pad(slideNumber, 2) + "/" + pad((numberOfSlides - 3), 2)
+itemIndex.innerText = zerosBefore(slideNumber, 2) + "/" + zerosBefore((numberOfSlides - 3), 2)
 
+
+
+/**
+ * Définis le numéro de la slide à afficher.
+ * @param {Number} slideNum 
+ */
 function setSlidesLeftAttribute(slideNum) {
     let items = document.getElementsByClassName("expertise-item")
 
@@ -15,22 +24,46 @@ function setSlidesLeftAttribute(slideNum) {
         item.style.left = slideNum * PIXELPERSLIDE + "px"
     }
 
-    itemIndex.innerText = pad(slideNum, 2) + "/" + pad((numberOfSlides - 3), 2)
+    itemIndex.innerText = zerosBefore(slideNum, 2) + "/" + zerosBefore((numberOfSlides - 3), 2)
 }
 
+/**
+ * Définis la couleur des flèches selon la slide affichée.
+ */
 function setArrowsColor() {
-    if(slideNumber == 1) expertiseLeftArrow.style.color = "gray"; 
-    else expertiseLeftArrow.style.color = "white";
-    if(slideNumber == numberOfSlides - 3) expertiseRightArrow.style.color = "gray"; 
-    else expertiseRightArrow.style.color = "white";
+    if(slideNumber == 1) {
+        expertiseLeftArrow.style.color = "gray"; 
+    } else {
+        expertiseLeftArrow.style.color = "white"; 
+    }
+
+    if(slideNumber === numberOfSlides - 3) {
+        expertiseRightArrow.style.color = "gray"; 
+    } else {
+        expertiseRightArrow.style.color = "white";
+    }
 }
 
-function pad(num, size) {
+
+/**
+ * Formate un nombre à une certaine taille. 
+ * Par exemple: 
+ *  zerosBefore(5, 2) -> "05"
+ *  zerosBefore(5, 10) -> 0000000005
+ * @param {Number} num Nombre
+ * @param {Number} size Nombre de caractères avec lesquels num doit être représenté
+ * @returns 
+ */
+function zerosBefore(num, size) {
     num = num.toString();
     while (num.length < size) num = "0" + num;
     return num;
 }
 
+
+/**
+ * Évènements
+ */
 document.getElementById("left").addEventListener("click", () => {
     if(slideNumber > 1) {
         slideNumber--;
