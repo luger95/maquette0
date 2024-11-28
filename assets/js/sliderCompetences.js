c968b36919a7a0b725416afcadcade7f9c29999d
Martin Guionniere <22301599t>
2024-11-28 09:32:22 +0100

    Fix suite à la merge request

--- assets/js/sliderCompetences.js
+++ assets/js/sliderCompetences.js
@@ -1,21 +1,14 @@
-const imgs = document.querySelectorAll('[data-img-url]');
 const indexCompetences = document.getElementById('competences_index');
-indexCompetences.innerHTML = `<span>01/</span>0${imgs.length}`;
-
-imgs.forEach((div) => {
-    div.style.backgroundImage = `url(\'${div.getAttribute('data-img-url')}\')`;
-});
-
 const slider = document.querySelector('#competences_slides');
 const arrowButtons = document.querySelectorAll('[data-index-change]');
 let competencesIndex = 0;
 
 function slide(nextIndex) {
     if (nextIndex < 0) nextIndex = 0;
-    if (nextIndex >= imgs.length) nextIndex = imgs.length-1;
-    slider.style.transform = `translateX(-${(nextIndex/imgs.length)*99}%)`;
+    if (nextIndex >= 7) nextIndex = 7;
+    slider.style.transform = `translateX(-${(nextIndex/8)*99}%)`;
     competencesIndex = nextIndex;
-    indexCompetences.innerHTML = `<span>0${competencesIndex+1}/</span>0${imgs.length}`; 
+    indexCompetences.innerHTML = `<span>0${competencesIndex+1}/</span>08`; 
 }
 
 arrowButtons.forEach((button) => {
